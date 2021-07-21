36c2e1e980c35656010e78297ffac0e8982d83a8
Matheus Fernandes <matheusfernandesgomes1@gmail.com>
2021-07-21 19:58:28 -0300

    segundo

--- 2a.py
+++ 2a.py
@@ -1,6 +1,50 @@
 import numpy as np
 import math
 import collections
+
+def graus(self):
+  maior = [0, 0]
+  menor = [0, 10000]
+  soma = 0
+  distribuicao = [0 for n in range(self.nVertices)]
+  frequencia = []
+
+ for i in range(self.nVertices):
+            aux = 0
+            distribuicao[len(self.grafo[i])] = distribuicao[len(self.grafo[i])] + 1
+            if maior[1] < len(self.grafo[i]):
+                maior[0] = i
+                maior[1] = len(self.grafo[i])
+            elif menor[1] > len(self.grafo[i]):
+                menor[0] = i
+                menor[1] = len(self.grafo[i])
+            soma = soma + len(self.grafo[i])
+        for i in range(self.nVertices):
+            if distribuicao[i] != 0:
+                frequencia.append((i, distribuicao[i] / self.nVertices))
+        media = float(soma) / float(5)
+        return (maior, menor, media, frequencia)
+
+#componentes conexas
+ef conexidade(self):
+  componente = [1 for i in range(self.nVertices)]
+  t = []
+  n = 0
+  for i in range(self.nVertices):
+    bit = 0
+    if componente[i] != 0:
+      busca = self.busca_peso(componente, i)
+      t.append(len(busca))
+      n = n + 1
+    for j in range(self.nVertices):
+      if componente[j] == 0:
+        bit = bit + 1
+      if bit == self.nVertices:
+        return (n, t)
+
+
+
+
 # Caso arquivo não exita ele é criado, somente leitura utilizar a (fazer try catch)
 nome=input("Informe o nome é o formato do arquivo: ")
 file = open(nome,'r')
@@ -79,6 +123,9 @@ print("Matriz de adjacencias: \n",matriz)
 
 
 #print("Lista de adjacencias: \n",lista)
+
+
+
 
 
 file.close()
